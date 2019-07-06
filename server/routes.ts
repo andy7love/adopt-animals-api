15d85d73b5f8a4d88606829d6b5313adc5a828a0
Andres Amor <andy.7love@gmail.com>
2019-07-06 12:17:11 -0300

    Updated routes to follow plural requirement of REST APIs standard.

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,5 @@
 import { Application } from 'express';
 import animalRouter from './api/controllers/animal/router';
 export default function routes(app: Application): void {
-  app.use('/api/v1/animal', animalRouter);
+  app.use('/api/v1/animals', animalRouter);
 };
