9c1d7b0d6a2da4c56c74c3a60d0e6e4cd6813b91
Andres Amor <andy.7love@gmail.com>
2019-07-06 14:36:03 -0300

    Fixed PORT, CORS and API URL on the example on the index.html.

--- server/common/server.ts
+++ server/common/server.ts
@@ -15,7 +15,7 @@ export default class ExpressServer {
   constructor() {
     const root = path.normalize(__dirname + '/../..');
     app.set('appPath', root + 'client');
-    app.use(cors());
+    app.use(cors({ origin: true }));
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '10mb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '10mb' }));
     app.use(cookieParser(process.env.SESSION_SECRET));
